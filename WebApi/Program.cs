5136018b60595f88da72659834402ae28c84d4ae
grc4kd <griffin.crane@outlook.com>
2024-06-25 22:12:02 -0500

    Use HTTPS only connections to WebApi.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,7 @@
-using Microsoft.AspNetCore;
+using DataContext;
+using DataContext.Models;
+using DataContext.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApi;
 
@@ -6,12 +9,61 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        BuildWebHost(args).Run();
-    }
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddControllers();
+        builder.Services.AddScoped<ICustomerAccountRepository, CustomerAccountRepository>();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-    // EF Core uses this method at design time to access the DbContext
-    public static IWebHost BuildWebHost(string[] args)
-        => WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddDbContext<CustomerAccountContext>(options => options.
+                UseSqlite(builder.Configuration.GetConnectionString("AccountContext"),
+                a => a.MigrationsAssembly("WebApi")));
+
+            // DEMO add seed data from demo project in Development environment only
+            var contextOptions = new DbContextOptionsBuilder<CustomerAccountContext>()
+                .UseSqlite(builder.Configuration.GetConnectionString("AccountContext"));
+            var context = new CustomerAccountContext(contextOptions.Options);
+
+            var customer = context.Customers.Find(5);
+            if (customer == null) {
+                customer = new Customer(5);
+                context.Customers.Add(customer);
+            }
+            
+            var customerAccount = context.Accounts.Find(17);
+            if (customerAccount == null) {
+                context.Accounts.Add(new CustomerAccount() {
+                    Id = 17,
+                    Customer = customer
+                });
+            }
+
+            context.SaveChanges();
+        }
+        else
+        {
+            builder.Services.AddDbContext<CustomerAccountContext>(options => options.
+                UseSqlite(builder.Configuration.GetConnectionString("ProductionAccountContext"),
+                a => a.MigrationsAssembly("WebApi")));
+        }        
+
+        var app = builder.Build();
+    
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }    
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();   
+    }
 }
